9a838e9bc5e0b8498d229180496c8807d6d2930b
LeeYoHan <o_o950306@naver.com>
2017-07-21 14:20:59 +0900

    커밋 투!

---
+++ Aproject/src/com/tacademy/date/A.java
@@ -0,0 +1,9 @@
+package com.tacademy.date;
+
+public class A {
+
+	public A() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- Aproject/src/com/tacademy/date/Data.java
+++ Aproject/src/com/tacademy/date/Data.java
@@ -1,6 +1,20 @@
 package com.tacademy.date;
 
 public class Data {
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Data [name=" + name + "]";
+	}
 
 	public Data() {
 		// TODO Auto-generated constructor stub
